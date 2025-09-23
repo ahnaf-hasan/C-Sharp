0a76434cbbac76ad366b3ce359ad30eba5f0db10
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-09-23 21:05:36 +0600

    second commit

---
+++ sum.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 33;
+        int num2 = 34;
+        int num3 = 35;
+
+        int sum = num1 + num2 + num3;
+
+        Console.WriteLine("The sum is: " + sum);
+    }
+}
