1448d575fe5128f1a15c895281b1098e2fbc25c1
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-10-02 20:00:26 +0600

    leap year or not

---
+++ year.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if (IsLeapYear(year))
+        {
+            Console.WriteLine($"{year} is a leap year.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a leap year.");
+        }
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        // Leap year 
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
