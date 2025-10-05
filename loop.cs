4f8e94f053484f768e0975c7d52845db1c33a18e
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-10-05 19:31:18 +0600

    loop.cs

---
+++ loop.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //loop from 1 to 25
+        for (int i = 1; i <= 25; i++)
+        {
+            Console.WriteLine("Number: " + i);
+        }
+    }
+}
